0b47a8d980aa5d2da77777dde5bf3b2180fba4fd
Tibo-Wild <thibault.leparc45@gmail.com>
2020-11-03 13:37:25 +0100

    Add carousel

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,12 +2,14 @@ import { Component } from 'react';
 import Navbar from '../Navbar';
 import Formevent from '../event/Formevent';
 import ServiceList from '../services/ServiceList';
+import ControlledCarousel from '../carousel/ControlledCarousel';
 
 export default class Home extends Component {
     render(){
         return (
             <main className="container">
               <Navbar />
+              <ControlledCarousel />
               <Formevent />
               <ServiceList />
             </main>
