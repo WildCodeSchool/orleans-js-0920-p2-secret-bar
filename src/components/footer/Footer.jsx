a5965054f771c3d9a7568f4f72a84fa4f3d85c4a
theo.atlani@gmail.com <theo.atlani@gmail.com>
2020-11-04 17:54:36 +0100

    fix css global + 'remove carrousel' temporarily

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,8 +5,8 @@ export default class Footer extends Component{
     render(){
         return(
             <div className="Footer">
-                <h5 className="Credits">©Wild Code School Orléans 2020 - All right reserved</h5>
-                <p className="Students">Theo A. - Guillaume C. - Thibault L - Jordy N. - Samia S.</p>
+                <h5 className="Credits">©Wild Code School Orléans JS 2020 - All right reserved</h5>
+                <p className="Students">Théo A. - Guillaume C. - Thibault L.P. - Jordy N. - Samia S.</p>
             </div>
         );
     }
