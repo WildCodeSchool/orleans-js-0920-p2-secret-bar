293596be87b158d9471d299c8d24e8d21eb5229f
Tibo-Wild <thibault.leparc45@gmail.com>
2020-11-05 14:35:19 +0100

    Add selection composant and css

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,16 +2,16 @@ import { Component } from 'react';
 import Navbar from '../Navbar';
 import Formevent from '../event/Formevent';
 import ServiceList from '../services/ServiceList';
-import ControlledCarousel from '../carousel/ControlledCarousel';
+import Selection from '../selection/Selection';
 
 export default class Home extends Component {
     render(){
         return (
             <main className="container">
-              <Navbar />
-              <ControlledCarousel />
-              <Formevent />
-              <ServiceList />
+                <Navbar />
+                <Selection />
+                <Formevent />
+                <ServiceList />
             </main>
         );
     }
