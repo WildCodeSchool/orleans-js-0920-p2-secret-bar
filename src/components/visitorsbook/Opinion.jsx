793cb7e55bb47b7f8228b35968bf4b53799d3d87
theo.atlani@gmail.com <theo.atlani@gmail.com>
2020-11-26 09:06:11 +0100

    fix desktop visitorsbook

--- src/components/visitorsbook/Opinion.jsx
+++ src/components/visitorsbook/Opinion.jsx
@@ -1,38 +1,22 @@
 import React from 'react';
 import './Opinion.css';
 
-
-
 const users = [
     {
-      name: 'Robert Reyes',
-      avatar: 'https://randomuser.me/api/portraits/men/28.jpg',
-      comment: 'splendide',
-      note:3,
+      name: 'Thibault LE PARC',
+      comment: ' splendide '
     },
     {
-      name: 'Nellie Caldwell',
-      comment: 'La soirée était formidable',
-      avatar: 'https://randomuser.me/api/portraits/women/17.jpg',
-      online: true,
+      name: 'Samia SAADANI',
+      comment: ' Quel ambiance ! J\'ai tellement dansé que je n\'ai même pas profité du reste... '
     },
     {
-      name: 'Vernon Mason',
-      comment: 'La soirée était formidable',
-      avatar: 'https://randomuser.me/api/portraits/men/84.jpg',
-      online: true
+      name: 'Guillaume COUTURIER',
+      comment: " j'ai bien cru que je n'allais jamais rentrer chez moi rejoindre ma femme, je suis tombé amoureux 5 fois. "
     },
     {
-      name: 'Erica Hunt',
-      comment: "j'ai bien cru que je n'allais jamais rentrer chez moi rejoindre ma femme, je suis tombé amoureux 5 fois.",
-      avatar: 'https://randomuser.me/api/portraits/women/87.jpg',
-      online: false
-    },
-    {
-      name: 'Juanita Phillips',
-      comment: 'je risque pas de louper la prochaine',
-      avatar: 'https://randomuser.me/api/portraits/women/55.jpg',
-      online: true
+      name: 'Théo ATLANI',
+      comment: ' Depuis cette soirée, je note tous leurs évenements dans mon agenda pour n\'en rater aucun ! '
     }
   ];
 
@@ -41,55 +25,53 @@ class Opinion extends React.Component{
         super(props);
         this.state = {
             online:false,
-            
         };
     }
     render(){
         return(
             <div>
-            <div className="tout">
-                <div className='noti'>
-                    
-                    <div className='text'>
-                        <div className="Note" id='note'>
-                            <span class="star">&#x2605;</span>
-                            <span class="star">&#x2605;</span>
-                            <span class="star">&#x2605;</span>
-                            <span class="star">&#x2605;</span>
+                <div className='notice first-comment'>
+                    <div className='att'>
+                        <div className='texte'>
+                            <div className="Note" id='note'>
+                                <span class="star">&#x2605;</span>
+                                <span class="star">&#x2605;</span>
+                                <span class="star">&#x2605;</span>
+                                <span class="star">&#x2605;</span>
+                            </div>
+                            <p className='citation'>"<i>j'ai passé une bonne soirée</i>"</p>
                         </div>
-                        <p className='quote'>"<i>j'ai passé une bonne soirée</i>"</p>
-                    </div>
-                    
                         <p className='blase'><small>jordy ngoma</small></p>
-                </div>
-            </div>
-            <div>
-            <div className={this.state.online ? "moreCaption":"status"}>
-                {users.map(item => (
-                <div className='notice'>
-                <div className='att'>
-                <div className='texte'>
-                    <div className="Note" id='note'>
-                        <span class="star">&#x2605;</span>
-                        <span class="star">&#x2605;</span>
-                        <span class="star">&#x2605;</span>
-                        <span class="star">&#x2605;</span>
-                        <span class="star">&#x2605;</span>
                     </div>
-                    <p className='citation'>"<i>{item.comment}</i>"</p>
                 </div>
+
+                <div>
+                    <div className={this.state.online ? "moreCaption":"status"}>
+                        {users.map(item => (
+                            <div className='notice'>
+                                <div className='att'>
+                                    <div className='texte'>
+                                        <div className="Note" id='note'>
+                                            <span class="star">&#x2605;</span>
+                                            <span class="star">&#x2605;</span>
+                                            <span class="star">&#x2605;</span>
+                                            <span class="star">&#x2605;</span>
+                                            <span class="star">&#x2605;</span>
+                                        </div>
+                                        <p className='citation'>"<i>{item.comment}</i>"</p>
+                                    </div>
+                                    <p className='blase'><small>{item.name}</small></p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
                 </div>
-                    <p className='blase'><small>{item.name}</small></p>
-                </div>
-                ))}
-            </div>
-            </div>
-            <button className="more" onClick = { event => {
-            const newOnline = !this.state.online;
-            this.setState({ online : newOnline });
-            }}
-            >{ this.state.online ? "voir moin":"voir plus"}</button>
-            
+                <button className="more" onClick = { () => {
+                    const newOnline = !this.state.online;
+                        this.setState({ online : newOnline });
+                    }}
+                    >{ this.state.online ? "voir moins":"voir plus"}
+                </button>
             </div>
         );
     }
